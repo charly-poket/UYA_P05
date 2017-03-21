c41071c56a38921931814089fd6b233a24588c97
charly-poket <alu0100844665@ull.edu.es>
2017-03-21 12:12:49 +0000

    link de ayuda importante

--- web.js
+++ web.js
@@ -6,3 +6,9 @@ app.use(express.static(__dirname + '/public'));
 var server=app.listen(8888,function(){
     console.log('Servidor web iniciado');
 });
+/*
+links de ayuda
+
+http://expressjs.com/es/starter/static-files.html
+
+*/
